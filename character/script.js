722828f8325073be71c348b0d2ce8f8a3fe72a86
Kevin Tan <140094426+kvintan@users.noreply.github.com>
2024-04-26 16:24:38 +0700

    Add files via upload

---
+++ character/script.js
@@ -0,0 +1,15 @@
+document.getElementById("homeFooter").addEventListener("click", function() {
+    window.location.href = "../homepage/index.html";
+});
+
+document.getElementById("characterFooter").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.getElementById("classFooter").addEventListener("click", function() {
+    window.location.href = "../dragonClass/index.html";
+});
+
+document.getElementById("islandFooter").addEventListener("click", function() {
+    window.location.href = "../island/index.html";
+});
